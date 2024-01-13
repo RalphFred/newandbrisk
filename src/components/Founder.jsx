c24658168f22f2df79d8a91a6066c2e43a2a8189
RalphFred <raphaelgbaorun@gmail.com>
2024-01-13 03:58:14 +0100

    build: built the pricing card

--- src/components/Founder.jsx
+++ src/components/Founder.jsx
@@ -1,16 +1,24 @@
-import founder from '../assets/founder.jpg'
+import founder from "../assets/founder.jpg";
 
 export default function Founder() {
-  return(
-    <div className='px-8 py-8 lg:pb-12 lg:px-32 bg-cream flex flex-col-reverse lg:flex-row items-center'>
-      <div className='w-full lg:w-1/2 mb-12 lg:mb-0'>
-        <img src={founder} className='w-full lg:w-4/5 lg:h-[600px]' />
+  return (
+    <div className="px-8 py-8 lg:pb-12 lg:px-32 bg-cream flex flex-col-reverse lg:flex-row items-center">
+      <div className="w-full lg:w-1/2 mb-12 lg:mb-0">
+        <img src={founder} className="w-full lg:w-4/5 lg:h-[600px]" />
       </div>
-      <div className='w-full lg:w-1/2 font-handlee text-xl'>
-        <p className='mb-4'> 
-        Meet <span className="font-semibold">CHEF NANA</span>, the <span className="font-bold">FOUNDER</span> and <span className="bold">CEO</span> of <span className="font-bold">New and Brisk</span>
+      <div className="w-full lg:w-1/2 font-handlee text-xl">
+        <p className="mb-4">
+          Meet <span className="font-semibold">CHEF NANA</span>
+          , the esteemed <span className="font-bold">FOUNDER</span> and
+          dedicated <span className="bold">CEO</span> of{" "}
+          <span className="font-bold">New and Brisk Culinary</span>. With more
+          than 10 years of valuable experience in the culinary arts, Chef Nana
+          brings a wealth of expertise to our kitchen, where tradition meets
+          innovation in delightful harmony. Her culinary journey is a reflection
+          of her passion and commitment to crafting delicious experiences for
+          our guests.
         </p>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,9 +1,9 @@
-import {services} from "../data";
+import link from '../assets/link.svg'
 
 export default function Pricing() {
   return (
     <div className="pt-[112px] px-8 lg:px-32 bg-cream border-none">
-      <h1 className="font-labrada font-bold text-5xl mb-4">Our Services</h1>
+      {/* <h1 className="font-labrada font-bold text-5xl mb-4">Our Services</h1>
       <p className="text-xl font-handlee lg:w-2/3">
         Here at New and Brisk Culinary, we take pride in offering a
         comprehensive array of services designed to cater to your passion for
@@ -35,7 +35,58 @@ export default function Pricing() {
             </div>
           </div>
         ))}
-      </div>
+      </div> */}
+      <h1 className="font-labrada font-bold text-5xl mb-16">Services & Pricing</h1>
+      <div className="flex justify-around items-center">
+        <div className="border-2 border-green lg:w-[350px] p-8 rounded-3xl text-center">
+          <button className="border-2 border-green rounded-md px-2 py-1 font-bold font-handlee mb-4 text-lg">01</button>
+          <h4 className="font-handlee font-bold mb-2">Apprenticeship Class</h4>
+          <h1 className="font-handlee font-bold text-4xl mb-4">₦100,000<span className="text-sm">NGN</span></h1>
+
+          <span className='font-handlee text-sm block mb-40'>Perfecting skills: hands-on practice, expert guidance in our Apprenticeship</span>
+
+          <div className="font-handlee font-bold bg-green text-cream px-6 py-2 rounded-full flex justify-between items-end">
+            <div className='text-lg'>
+              Get Started  
+            </div>  
+
+            <img src={link} alt="icon" />
+          </div>
+        </div>
+
+        <div className="border-2 border-green lg:w-[350px] p-8 rounded-3xl text-center">
+          <button className="border-2 border-green rounded-md px-2 py-1 font-bold font-handlee mb-4 text-lg">02</button>
+          <h4 className="font-handlee font-bold mb-2">Food Enthisiast</h4>
+          <h1 className="font-handlee font-bold text-4xl mb-4">₦200,000<span className="text-sm">NGN</span></h1>
+
+          <span className='font-handlee text-sm block mb-40'>A perfect blend of hands-on experience and expert guidance</span>
+
+          <div className="font-handlee font-bold bg-green text-cream px-6 py-2 rounded-full flex justify-between items-end">
+            <div className='text-lg'>
+              Get Started  
+            </div>  
+
+            <img src={link} alt="icon" />
+          </div>
+        </div>
+
+        <div className="border-2 border-green lg:w-[350px] p-8 rounded-3xl text-center">
+          <button className="border-2 border-green rounded-md px-2 py-1 font-bold font-handlee mb-4 text-lg">03</button>
+          <h4 className="font-handlee font-bold mb-2">Professional Home</h4>
+          <h1 className="font-handlee font-bold text-4xl mb-4">₦350,000<span className="text-sm">NGN</span></h1>
+
+          <span className='font-handlee text-sm block mb-40'>Elevate your cooking at Professional Home: expert guidance, hands-on excellence.</span>
+
+          <div className="font-handlee font-bold bg-green text-cream px-6 py-2 rounded-full flex justify-between items-end">
+            <div className='text-lg'>
+              Get Started  
+            </div>  
+
+            <img src={link} alt="icon" />
+          </div>
+        </div>
+
+       </div>
     </div>
   );
 }
